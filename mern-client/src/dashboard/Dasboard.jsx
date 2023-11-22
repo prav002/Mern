fd0dff9af94291e7174e6ebff91557213960b8f4
prav002 <pravinkumar.r2021@vitstudent.ac.in>
2023-11-22 19:17:38 +0530

    first commit

---
+++ mern-client/src/dashboard/Dasboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dasboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dasboard
---
+++ mern-client/src/dashboard/SideBar.jsx
@@ -0,0 +1,54 @@
+import { Sidebar } from 'flowbite-react';
+import { BiBuoy } from 'react-icons/bi';
+import { HiArrowSmRight, HiChartPie, HiInbox, HiOutlineCloud, HiOutlineCloudUpload, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+
+import userImg from "../assets/profiles.jpg"
+const SideBar = () => {
+  return (
+    <div>
+    <Sidebar aria-label="Sidebar with content separator example">
+    <Sidebar.Logo href="#" img={userImg} imgAlt="Flowbite logo">
+        Flowbite
+      </Sidebar.Logo>
+    <Sidebar.Items>
+      <Sidebar.ItemGroup>
+        <Sidebar.Item href="/admin/dashboard" icon={HiChartPie}>
+          Dashboard
+        </Sidebar.Item>
+        <Sidebar.Item href="/admin/dashboard/upload" icon={HiOutlineCloudUpload}>
+          Upload E-books
+        </Sidebar.Item>
+        <Sidebar.Item href="/admin/dashboard/manage" icon={HiInbox}>
+          Manage E-Books
+        </Sidebar.Item>
+        <Sidebar.Item href="#" icon={HiUser}>
+          Users
+        </Sidebar.Item>
+        <Sidebar.Item href="#" icon={HiShoppingBag}>
+          Products
+        </Sidebar.Item>
+        <Sidebar.Item href="/login" icon={HiArrowSmRight}>
+          Sign In
+        </Sidebar.Item>
+        <Sidebar.Item href="/logout" icon={HiTable}>
+          Sign Out
+        </Sidebar.Item>
+      </Sidebar.ItemGroup>
+      <Sidebar.ItemGroup>
+        <Sidebar.Item href="#" icon={HiChartPie}>
+          Upgrade to Pro
+        </Sidebar.Item>
+        <Sidebar.Item href="#" icon={HiViewBoards}>
+          Documentation
+        </Sidebar.Item>
+        <Sidebar.Item href="#" icon={BiBuoy}>
+          Help
+        </Sidebar.Item>
+      </Sidebar.ItemGroup>
+    </Sidebar.Items>
+  </Sidebar>
+  </div>
+  )
+}
+
+export default SideBar
